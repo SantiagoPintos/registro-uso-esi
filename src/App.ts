ac38d862067f05912717af6a741b9e94927f07fb
Santiago Pintos <spintos@outlook.es>
2023-07-14 23:13:57 -0300

    Template
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

---
+++ src/App.ts
@@ -0,0 +1,44 @@
+import { app, BrowserWindow, ipcMain, nativeTheme } from "electron";
+import path from "path";
+
+const isDev:boolean = true;
+const createWindow = async ():Promise<void> => {
+    const win = new BrowserWindow({
+        height: 600,
+        width: 800,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js")
+        }
+    });
+
+    win.loadFile(path.join(__dirname, "index.html"));
+
+    if (isDev) {
+        win.webContents.openDevTools;
+    }
+
+    ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+        nativeTheme.themeSource = 'light'
+    } else {
+        nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+    })
+
+    ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+    })
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on("activate", () => {
+        if(BrowserWindow.getAllWindows().length === 0) createWindow();
+    })
+});
+
+app.on("window-all-closed",async () => {
+    if(process.platform !== "darwin") app.quit();    
+})
