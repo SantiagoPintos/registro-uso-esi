4b91ff91b2efb7717504441c86969c7b6c6e1261
Santiago Pintos <spintos@outlook.es>
2023-11-08 17:15:44 -0300

    dbManager: throw error instead of writing it to node console
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/dbManager/dbConnection.ts
+++ src/dbManager/dbConnection.ts
@@ -5,12 +5,9 @@ let db: sqlite3.Database|null = null;
 export function databaseConnector(){
     if(db===null){
         db = new sqlite3.Database('./src/database/database.sqlite', (err: Error|null) =>{
-            if (err){
-                console.error(err)
-            } else {
-                console.log('Conexion exitosa a db');
-            }
-    
+            if (err) throw new Error(err.message);
+            
+            console.log('Conexion exitosa a db');
         });
     }
     return db;
@@ -19,7 +16,7 @@ export function databaseConnector(){
 export function closeConnection(){
     if(db!==null){
         db.close((err: Error|null) => {
-            if (err) console.error(err.message);
+            if (err) throw new Error(err.message);
 
             console.log('Conexion a db cerrada')
         })
--- src/dbManager/dbOperator.ts
+++ src/dbManager/dbOperator.ts
@@ -4,7 +4,7 @@ export function createData(db: Database){
     const createQuery = "CREATE TABLE alumnos (ci INTEGER PRIMARY KEY, nombre TEXT, grupo TEXT)";
 
     db.run(createQuery, [], function(err: Error|null){
-        if (err) console.error(err);
+        if (err) throw new Error(err.message);
         console.log('Tabla creada');
     })
 }
