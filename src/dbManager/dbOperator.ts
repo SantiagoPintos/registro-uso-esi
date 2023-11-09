e527b3e583cd2d35f18df854bdc018da6af34b83
Santiago Pintos <spintos@outlook.es>
2023-11-09 16:01:18 -0300

    Create function to insert data into table 'alumnos'
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/dbManager/dbOperator.ts
+++ src/dbManager/dbOperator.ts
@@ -1,10 +1,19 @@
 import { Database } from 'sqlite3';
 
 export function createData(db: Database){
-    const createQuery = "CREATE TABLE IF NOT EXISTS alumnos (ci INTEGER PRIMARY KEY, nombre TEXT, grupo TEXT)";
+    const createQuery = "CREATE TABLE IF NOT EXISTS alumnos (ci INTEGER PRIMARY KEY, nombre TEXT NOT NULL, grupo TEXT NOT NULL, hora DATETIME NOT NULL)";
 
     db.run(createQuery, [], function(err: Error|null){
         if (err) throw new Error(err.message);
         console.log('Tabla creada');
+    })
+}
+
+export function insertAlumno(db: Database, data: Alumno){
+    const insertQuery = "INSERT INTO alumno (ci, nombre, grupo, hora) VALUES (?, ?, ?, ?)";
+
+    db.run(insertQuery, [data.ci, data.nombre, data.grupo, data.hora], function(err: Error|null){
+        if (err) throw new Error(err.message);
+        console.log('Alumno insertado');
     })
 }
---
+++ src/types/dbOperator.d.ts
@@ -0,0 +1,10 @@
+declare global {
+    interface Alumno{
+        ci: string,
+        nombre: string,
+        grupo: string,
+        hora: Date
+    }
+}
+
+export {};
