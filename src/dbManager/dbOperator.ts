b3e449961ff3b8f57c5d06e9a577b2d734be62a6
Santiago Pintos <spintos@outlook.es>
2023-11-13 14:43:19 -0300

    dbOperator: guard table creation statements inside try/catch
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/dbManager/dbOperator.ts
+++ src/dbManager/dbOperator.ts
@@ -4,19 +4,24 @@ export function createData(db: Database){
     const createQueryGrupo= "CREATE TABLE IF NOT EXISTS Grupo(nombre TEXT PRIMARY KEY)";
     const createQueryAlumnos = "CREATE TABLE IF NOT EXISTS alumnos (ci TEXT PRIMARY KEY, nombre TEXT NOT NULL, grupo TEXT, FOREIGN KEY(grupo) REFERENCES Grupo(nombre))";
     const createQueryRegistro= "CREATE TABLE IF NOT EXISTS registro (alumno TEXT PRIMARY KEY, entrada DATETIME NOT NULL, salida DATETIME NOT NULL, FOREIGN KEY (alumno) REFERENCES alumnos(ci))";
-    
-    db.run(createQueryGrupo, [], function(err: Error|null){
-        if (err) throw new Error(err.message);
-        console.log('Tabla grupos creada');
-    })
-    db.run(createQueryAlumnos, [], function(err: Error|null){
-        if (err) throw new Error(err.message);
-        console.log('Tabla alumnos creada');
-    })
-    db.run(createQueryRegistro, [], function(err: Error|null){
-        if (err) throw new Error(err.message);
-        console.log('Tabla registro creada');
-    })
+
+    try {
+        db.run(createQueryGrupo, [], function(err: Error|null){
+            if (err) throw new Error(err.message);
+            console.log('Tabla grupos creada');
+        })
+        db.run(createQueryAlumnos, [], function(err: Error|null){
+            if (err) throw new Error(err.message);
+            console.log('Tabla alumnos creada');
+        })
+        db.run(createQueryRegistro, [], function(err: Error|null){
+            if (err) throw new Error(err.message);
+            console.log('Tabla registro creada');
+        })
+    } catch (err){
+        throw new Error('Imposible crear tablas de base de datos'+ err);
+    }
+
 }
 
 export function insertAlumno(db: Database, data: Alumno){
