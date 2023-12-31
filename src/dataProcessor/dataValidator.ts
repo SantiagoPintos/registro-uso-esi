db1afed1eb53db9cfdce6a972ad559247985f014
Santiago Pintos <spintos@outlook.es>
2023-12-31 12:26:24 -0300

    Improve error messages in some scenarios
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/dataProcessor/dataValidator.ts
+++ src/dataProcessor/dataValidator.ts
@@ -4,7 +4,7 @@ import { databaseConnector, closeConnection } from "../dbManager/dbConnection";
 
 export function validateData(ci: string):void{
     //validate if ci is a valid uruguayan ci
-    if(!ciValidator(ci)) throw new Error("CI invalida");
+    if(!ciValidator(ci)) throw new Error("CI inválida");
     const db = databaseConnector();
     try{
         const isRegistered = isInDB(db, ci);
--- src/studentManager/utils/getStudentFromCI.ts
+++ src/studentManager/utils/getStudentFromCI.ts
@@ -7,7 +7,7 @@ import * as sqlite3 from 'sqlite3';
 export async function getStudentFromCi(ci: string): Promise<Alumno>{
     let db: sqlite3.Database|null = null;
     try{
-        if(!ciValidator(ci)) throw new Error("CI invalida");
+        if(!ciValidator(ci)) throw new Error("CI inválida");
         db = databaseConnector();
         return await getAlumnoFromCi(ci, db);
     } catch (e:any) {
