98ec62d3275a8a271f7942dc7dfe58e00058055c
Santiago Pintos <spintos@outlook.es>
2023-12-31 12:14:51 -0300

    getStudentFromCi: fixed SQLITE_MISUSE error when trying to close a non-existent connection to the database
    When the student's ci was invalid, "ciValidator" threw an error before establishing the connection to the database, the "catch" captured that message and the "finally" block tried to close the connection to the database without verifying that it was active.
    
    Signed-off-by: Santiago Pintos <spintos@outlook.es>

--- src/studentManager/utils/getStudentFromCI.ts
+++ src/studentManager/utils/getStudentFromCI.ts
@@ -1,17 +1,22 @@
-import { getAlumnoFromCi } from "./../../dbManager/dbOperator";
+import { getAlumnoFromCi } from "../../dbManager/dbOperator";
 import { ciValidator } from "../../dataProcessor/ciValidator";
-import { databaseConnector, closeConnection } from "../../dbManager/dbConnection";
+import { closeConnection, databaseConnector } from "../../dbManager/dbConnection";
 import { debug } from "../../App";
+import * as sqlite3 from 'sqlite3';
 
 export async function getStudentFromCi(ci: string): Promise<Alumno>{
+    let db: sqlite3.Database|null = null;
     try{
         if(!ciValidator(ci)) throw new Error("CI invalida");
-        const db = databaseConnector();
-        const alumno: Alumno = await getAlumnoFromCi(ci, db);
-        closeConnection();
-        return alumno;
+        db = databaseConnector();
+        return await getAlumnoFromCi(ci, db);
     } catch (e:any) {
         if(debug) console.error('getStudentFromCi: '+e.message);
         throw new Error(e.message);
+    }
+    finally {
+        if(db){
+            closeConnection();
+        }
     }
 }
